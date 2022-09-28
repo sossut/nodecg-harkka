7cdd16a033616178aecfb1a2700fa7e96d15f1c3
sossut <mikki.alakari@elisanet.fi>
2022-09-28 14:01:29 +0300

    123

---
+++ extension/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function (nodecg) {
+	nodecg.Replicant("speakerReplicant", { defaultValue: "Maija Mehiläinen" });
+	nodecg.Replicant("titleReplicant", {defaultValue: "title"});
+};
